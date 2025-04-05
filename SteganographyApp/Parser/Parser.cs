51054d6da3bbd1164612d248c9de15c6e5e58fe7
cromeros <c.romerosanchez@wsu.edu>
2025-04-05 02:42:28 -0700

    Created password parser and password validator to make sure that the password is created is a valid password

---
+++ SteganographyApp/Parser/Parser.cs
@@ -0,0 +1,33 @@
+﻿using System
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteganographyApp.Parser
+{
+    internal class Parser
+    {
+        static string Encoding(string data)
+        {
+            string readableData = "";
+
+            string metadata = new string(data.Where(c => !char.IsWhiteSpace(c)).ToArray());
+
+            foreach (char c in metadata)
+            {
+                readableData += GetPrintableAscii(c);
+            }
+
+            return readableData;
+        }
+
+        static char GetPrintableAscii(int n)
+        {
+            int start = 33;
+            int total = 122;
+            int asciiValue = start + (n % total);
+            return (char)asciiValue;
+        }
+    }
+}
