a0fd4ea6cb34f58d5448a1f69e6e46c20f7f6d23
Nasen Wilson <32120172+nasenw@users.noreply.github.com>
2025-04-04 20:25:44 -0700

    Create PasswordCreator

---
+++ SteganographyApp/PasswordCreator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteganographyApp
+{
+    public class PasswordCreator
+    {
+        public int getRandomLocation(int fileLength, int passwordSize)
+        {
+            Random random = new Random();
+            return random.Next(0, fileLength - passwordSize);
+        }
+
+        public string getPassword(string data, int passwordLength)
+        {
+            int startIndex = getRandomLocation(0, data.Length);
+
+            string currPassword = "";
+
+            while (currPassword.Length < passwordLength)
+            {
+                currPassword += data[startIndex];
+                startIndex++;
+            }
+
+            // If not valid password
+            //getPassword(data, passwordLength);
+
+            return currPassword;
+        }
+    }
+}
