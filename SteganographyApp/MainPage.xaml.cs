539fd4b124e4bbbfecc92d14a9cfc7c6379c9c31
Nasen Wilson <32120172+nasenw@users.noreply.github.com>
2025-04-05 00:13:26 -0700

    Fixed Merge

--- SteganographyApp/MainPage.xaml.cs
+++ SteganographyApp/MainPage.xaml.cs
@@ -1,14 +1,9 @@
-﻿
-
-
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace SteganographyApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-        string password = "stop goonin";
 
         public MainPage()
         {
@@ -40,43 +35,7 @@ namespace SteganographyApp
 
 
 
-        // Event handling the uploaded image
-        private async void UploadBtn_Clicked(object sender, EventArgs e)
-        {
-            try
-            {
-                var result = await FilePicker.Default.PickAsync(new PickOptions
-                {
-                    PickerTitle = "Select an Image",
-                    FileTypes = FilePickerFileType.Images
-                });
-
-                if (result != null)
-                {
-                    using var originalStream = await result.OpenReadAsync();
-
-                    var memoryStream = new MemoryStream();
-                    await originalStream.CopyToAsync(memoryStream);
-                    memoryStream.Position = 0;
-
-                    MyImageControl.Source = ImageSource.FromStream(() => memoryStream);
-                }
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert("Error", ex.Message, "OK");
-            }
-        }
-
-        private async void CopyPassword_Clicked(object sender, EventArgs e)
-        {
-            
-
-            await Clipboard.Default.SetTextAsync(password);
-
-            await DisplayAlert("Copied", "New Passwored has been copied to clipboard! 👉👈", "OK");
-
-        }
+        
         // private async Task ClearClipboard() =>await Clipboard.Default.SetTextAsync(null);
     }
 
