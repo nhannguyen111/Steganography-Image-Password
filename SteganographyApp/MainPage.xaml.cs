c6b149971956ef9740f38d71f37a8ba43af11101
jasoneigemanwsu <117321638+jasoneigemanwsu@users.noreply.github.com>
2025-04-04 21:21:15 -0700

    Picture preview and file upload

--- SteganographyApp/MainPage.xaml.cs
+++ SteganographyApp/MainPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace SteganographyApp
+﻿
+
+
+using System.Threading.Tasks;
+
+namespace SteganographyApp
 {
     public partial class MainPage : ContentPage
     {
@@ -19,6 +24,40 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        // Event handling the uploaded image
+        private async void UploadBtn_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var result = await FilePicker.Default.PickAsync(new PickOptions
+                {
+                    PickerTitle = "Select an Image",
+                    FileTypes = FilePickerFileType.Images
+                });
+
+                if (result != null)
+                {
+                    using var originalStream = await result.OpenReadAsync();
+
+                    var memoryStream = new MemoryStream();
+                    await originalStream.CopyToAsync(memoryStream);
+                    memoryStream.Position = 0;
+
+                    MyImageControl.Source = ImageSource.FromStream(() => memoryStream);
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+
+        private void CopyPassword_Clicked(object sender, EventArgs e)
+        {
+            //TODO: Copy new password to clipboard
+
         }
     }
 
